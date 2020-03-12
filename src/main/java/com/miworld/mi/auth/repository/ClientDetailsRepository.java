a582d0193aa0b2709b4284898ffda0e625947da3
vishnu <vishnu@creatorvisualmedia.com>
2020-03-12 09:36:02 +0530

    commit1

---
+++ src/main/java/com/miworld/mi/auth/repository/ClientDetailsRepository.java
@@ -0,0 +1,13 @@
+package com.miworld.mi.auth.repository;
+
+import com.miworld.mi.auth.model.ClientSystemDetails;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClientDetailsRepository extends MongoRepository<ClientSystemDetails, Long> {
+
+    public Optional<ClientSystemDetails> findByClientId(String clientId);
+}
